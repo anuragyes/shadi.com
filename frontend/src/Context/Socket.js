f79bd8df2819b32f59a94ad09552866bb6c368c9
Anurag pandey <149105080+anuragyes@users.noreply.github.com>
2025-10-29 18:29:53 +0530

    Update SOCKET_URL to production backend

--- frontend/src/Context/Socket.js
+++ frontend/src/Context/Socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = "http://localhost:5000"; // your backend URL
+const SOCKET_URL = "https://shadii-com.onrender.com"; // your backend URL
 
 const socket = io(SOCKET_URL, {
   withCredentials: true,
